89548ad9a0c75f0ce4f4075f00e1090fdfbeff16
Crawford Denys arryn <cdenys-a@il-i6.21-school.ru>
2019-06-10 17:48:42 +0300

    argv sort added

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thaley <thaley@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cdenys-a <cdenys-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/30 14:01:27 by thaley            #+#    #+#             */
-/*   Updated: 2019/06/10 13:53:44 by thaley           ###   ########.fr       */
+/*   Updated: 2019/06/10 17:46:09 by cdenys-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,29 @@ int		parse_validate_flags(t_flags *flag, char *arg)
 	return (1);
 }
 
+void	argv_sort(int argc, char **argv, int i, int order)
+{
+	char	*tmp;
+	int		i_mem;
+
+	i_mem = i;
+	while (i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) * order > 0)
+		{
+			tmp = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = tmp;
+			i = i_mem;
+		}
+		i++;
+	}
+}
+
 int		main(int argc, char **argv)
 {
 	int		i;
 	t_flags	*flag;
-	char	wrong_flag[3];
 
 	i = 1;
 	flag = create_flag();
@@ -57,6 +75,7 @@ int		main(int argc, char **argv)
 		parse_validate_flags(flag, argv[i]);
 		i++;
 	}
+	argv_sort(argc, argv, i, flag->r ? -1 : 1);
 	ls(argv, flag, i);
 	return (0);
 }
